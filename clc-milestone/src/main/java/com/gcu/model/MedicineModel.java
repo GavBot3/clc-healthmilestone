82d84319dba5b923d1eb38d563ce850447fc5085
Gavin Kuehn <gdkuehn03@gmail.com>
2023-10-20 17:50:35 -0700

    Created MedicineModel

---
+++ clc-milestone/src/main/java/com/gcu/model/MedicineModel.java
@@ -0,0 +1,13 @@
+package com.gcu.model;
+
+public class MedicineModel {
+
+	public String Type;
+	public String Strength;
+	public int Quantity;
+	public int NumServings;
+	public MedicineModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
