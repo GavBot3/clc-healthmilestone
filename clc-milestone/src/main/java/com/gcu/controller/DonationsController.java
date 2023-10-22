ca8a820aadbd4d1f9de74fe4b5c69f1263bf9725
Gage-Price <gprice11@my.gcu.edu>
2023-10-22 16:15:57 -0700

    Got it

--- clc-milestone/src/main/java/com/gcu/controller/DonationsController.java
+++ clc-milestone/src/main/java/com/gcu/controller/DonationsController.java
@@ -14,15 +14,6 @@ public class DonationsController {
     	model.addAttribute("title", "Donations");
         return "donations";
     }
-	//public ModelAndView display(Model model) {
-		/*ModelAndView mv = new ModelAndView();
 
-		// Add attributes to the Spring Model object
-		model.addAttribute("title", "Login Form"); // Add a "title" attribute with the value "Login Form"
-		model.addAttribute("loginModel", new LoginModel()); // Add a "loginModel" attribute with a new instance of LoginModel
-
-		mv.addObject(model); // Add the Model object to the ModelAndView
-		mv.setViewName("login"); // Set the view name to "login" (a reference to a JSP or Thymeleaf template)
-		return mv; // Return the ModelAndView object*/
-	}
+}
 
