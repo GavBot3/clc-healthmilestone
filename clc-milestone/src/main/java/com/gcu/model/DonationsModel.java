bbc9384150ed466efbc37d3e1dd3b0ee04a25016
Kahler35 <KahlerLitynski@gmail.com>
2023-11-05 16:08:20 -0700

    yolo swag

--- clc-milestone/src/main/java/com/gcu/model/DonationsModel.java
+++ clc-milestone/src/main/java/com/gcu/model/DonationsModel.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class DonationsModel {
-    @NotNull(message = "ID is a required field") // First name must not be null
-    @Size(min = 1, max = 32, message = "ID must be and int") // First name must be between 1 and 32 characters in length
+   // @NotNull(message = "ID is a required field") // First name must not be null
+    //@Size(min = 1, max = 32, message = "ID must be and int") // First name must be between 1 and 32 characters in length
     private int ID;
     
     @NotNull(message = "Organ is a required field") // Last name must not be null
