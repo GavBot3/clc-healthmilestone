e964844320e5c0a78433c010d5fd7b5d9d504ab4
Kahler35 <KahlerLitynski@gmail.com>
2023-12-10 14:39:42 -0700

    trying with Oauth

--- clc-milestone/src/main/java/com/gcu/SecurityConfig.java
+++ clc-milestone/src/main/java/com/gcu/SecurityConfig.java
@@ -10,50 +10,75 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
 import com.gcu.business.UserService;
 
 @Configuration
-@EnableWebSecurity	
+@EnableWebSecurity
 public class SecurityConfig {
 
 	@Autowired
 	private UserService service;
-	
+
 	PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-				http
-			.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/", "/images/**", "/service/**", "/home", "/login", "/register/**", "/loginsubmit", "/registersubmit" ).permitAll()
-					//.requestMatchers("/service/getdonation").authenticated()
-					.anyRequest().authenticated())
-			.formLogin(form -> form
-				.loginPage("/login")
-				.usernameParameter("username")
-				.passwordParameter("password")
-				.permitAll()
-				.defaultSuccessUrl("/dashboard", true))
-			.logout(lo -> lo
-					.logoutUrl("/logout")
-					.invalidateHttpSession(true)
-					.clearAuthentication(true)
-					.permitAll()
-					.logoutSuccessUrl("/"));
-		return http.build();	
+		http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/", "/images/**", "/service/**", "/home", "/login", "/register/**",
+								"/loginsubmit", "/registersubmit")
+						.permitAll()
+						// .requestMatchers("/service/getdonation").authenticated()
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.usernameParameter("username")
+						.passwordParameter("password")
+						.permitAll()
+						.defaultSuccessUrl("/dashboard", true))
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.invalidateHttpSession(true)
+						.clearAuthentication(true)
+						.permitAll()
+						.logoutSuccessUrl("/"));
+		return http.build();
 	}
-	
 
 	@Autowired
-	public void configure (AuthenticationManagerBuilder auth) throws Exception {
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		System.out.println("Configuring authentication manager");
 		auth.userDetailsService(service).passwordEncoder(passwordEncoder);
-		
+
 	}
 
+	private String clientId = "41efe9a337e083ade3da";
+	private String clientSecert = "498fece8b66b36a0dda8369e07f71b491e4737bd";
+	private String redirectUri = "http://localhost:8080/login/oauth2/code/github";
+
+	@Bean
+	public ClientRegistrationRepository clientRegistrationRepository() {
+		ClientRegistration registration = ClientRegistration.withRegistrationId("github")
+				.clientId(clientId)
+				.clientSecret(clientSecert)
+				.redirectUri(redirectUri)
+				.authorizationUri("https://github.com/login/oauth/authorize")
+				.tokenUri("https://github.com/login/oauth/access_token")
+				.userInfoUri("https://api.github.com/user")
+				.userNameAttributeName("id")
+				.clientName("GitHub")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE).build();
+		return new InMemoryClientRegistrationRepository(registration);
+	}
 
 }
